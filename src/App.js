0fab3fe0623a59df974e7f92efa5e2ba0e5e1994
imran-forloops <imran.ali@forloops.co>
2024-10-30 17:22:37 +0500

    latest changes

--- src/App.js
+++ src/App.js
@@ -7,32 +7,67 @@ import scrollToTop from "./helpers/scrollToTop";
 
 function App() {
   useEffect(() => {
-    scrollToTop()
-  },[])
+    scrollToTop();
+  }, []);
   return (
     <div className="relative">
-      <VideoBanner/>
-      <h2 className="text-center py-4 text-6xl text-red-900">Ottawa Barbershop</h2>
-      <p className="italic text-center text-xl">We provide the freshest cuts in the city.</p>
+      <VideoBanner />
+      <h2 className="text-center py-4 text-6xl text-red-900">Style x Studio</h2>
+      <p className="italic text-center text-xl">
+        We provide the freshest cuts in the city.
+      </p>
       <section className="py-4">
-        <ImageCarousel/>
+        <ImageCarousel />
       </section>
       <section className="py-4 gap-4 flex justify-center relative text-neutral-950">
-        <h2 className="text-9xl hidden lg:block absolute top-52 -left-48 text-red-900 rotate-90">About Us</h2>
+        <h2 className="text-9xl hidden lg:block absolute top-52 -left-48 text-red-900 rotate-90">
+          About Us
+        </h2>
         <div className="flex flex-col gap-4 px-4">
-        <p className="text-4xl">Who We are</p>
-        <p className="italic">Ottawa | Since 2023</p>
-        <p className="max-w-lg mx-auto text-xl">
-        Your go-to destination for top-notch grooming and hairstyling services in the heart of Ottawa. Established with a passion for the art of barbering and a commitment to our community, we are dedicated to providing you with an exceptional grooming experience that leaves you looking and feeling your best.
-        </p>
-        <p className="max-w-lg mx-auto text-xl">Founded in 2023, Billy's Barber was born out of a lifelong dream and dedication to the craft of barbering. Billy, our founder and master barber, envisioned a place where everyone could experience the timeless art of grooming in a comfortable and welcoming environment.</p>
-        <p className="max-w-lg mx-auto text-xl">Our team of experienced and skilled barbers is the backbone of our success. We take pride in our talented and diverse staff, each of whom is committed to delivering the highest level of service. Our barbers are not just professionals; they are artists who continually refine their skills and stay at the forefront of industry trends.</p>
+          <p className="text-4xl">Who We are</p>
+          <p className="italic text-2xl font-semibold">
+            Style X Studio | Since 2021
+          </p>{" "}
+          <p className="max-w-lg mx-auto text-base sm:text-xl leading-relaxed">
+            {" "}
+            Style X Studio, situated on 6th Road, Satellite Town, Rawalpindi, is
+            your premier destination for contemporary and personalized grooming
+            and hairstyling services. Located at D27, Dar Street, 6th Road,
+            Rawalpindi, we are committed to providing an exceptional salon
+            experience that leaves you looking and feeling your best.{" "}
+          </p>{" "}
+          <p className="max-w-lg mx-auto text-base sm:text-xl leading-relaxed">
+            {" "}
+            Established in 2021, Style X Studio was born from a lifelong passion
+            for the art of barbering. Our founder envisioned a space where
+            everyone could experience the timeless craft of grooming in a
+            relaxed and welcoming environment.{" "}
+          </p>{" "}
+          <p className="max-w-lg mx-auto text-base sm:text-xl  leading-relaxed">
+            {" "}
+            Our expert team of stylists and barbers is the foundation of our
+            success. We take immense pride in our talented and diverse staff,
+            who are not only professionals but artists dedicated to delivering
+            exceptional service. Each stylist continually hones their skills and
+            stays at the cutting edge of industry trends to ensure every client
+            receives a personalized and stylish experience.{" "}
+          </p>{" "}
+          <p className="max-w-lg mx-auto text-base sm:text-xl  leading-relaxed">
+            {" "}
+            Whether you're looking for a trend-setting haircut, rejuvenating spa
+            treatments, or expert grooming services, Style X Studio blends
+            modern aesthetics with skilled craftsmanship to ensure you leave
+            feeling refreshed, confident, and perfectly styled.{" "}
+          </p>
         </div>
-        <img className="w-1/4 grayscale object-cover hidden md:block" src='https://images.pexels.com/photos/1895701/pexels-photo-1895701.jpeg'></img>
+        <img
+          className="w-1/4 grayscale object-cover hidden md:block"
+          src="https://images.pexels.com/photos/1895701/pexels-photo-1895701.jpeg"
+        ></img>
       </section>
       <section className="w-xl pb-4 px-4 bg-red-950 bg-opacity-10 mt-4 flex flex-col md:flex-row mx-auto justify-center items-center gap-12">
-        <BarberServices/>
-        <SalonServices/>
+        <BarberServices />
+        <SalonServices />
       </section>
     </div>
   );
--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import barbervid from './videos/barbervid.mp4'
-import { Link } from 'react-router-dom'
+import React from "react";
+import barbervid from "./videos/barbervid.mp4";
+import { Link } from "react-router-dom";
 function VideoBanner() {
-
   return (
-    <div className='relative px-4 h-[75vh] bg-neutral-900 flex flex-col gap-4 justify-center items-center'>
-    <h2 className='z-20 text-neutral-300 text-3xl text-center'>Looking Your Best Never Goes Out of Style.</h2>
-    <Link to='/reservations' className='bg-red-900 nav z-20 text-neutral-300 text-xl px-4 py-2 font-bold block'>Book Now</Link>
-    <video className='absolute opacity-60 top-0 left-0 w-full h-full object-cover' src={barbervid} muted loop autoPlay></video>
+    <div className="relative px-4 h-[75vh] bg-neutral-900 flex flex-col gap-4 justify-center items-center">
+      <h2 className="z-20 text-neutral-300 text-3xl text-center">
+        Looking Your Best Never Goes Out of Style.
+      </h2>
+      <h3 className="z-20 text-neutral-300 text-base sm:text-3xl px-4 py-2 font-bold block">
+        Contact : +92 334 4844545
+      </h3>
+      <Link
+        to="/reservations"
+        className="bg-red-900 nav z-20 text-neutral-300 text-base sm:text-xl px-4 py-2 font-bold block"
+      >
+        Book Now
+      </Link>
+      <video
+        className="absolute opacity-60 top-0 left-0 w-full h-full object-cover"
+        src={barbervid}
+        muted
+        loop
+        autoPlay
+      ></video>
     </div>
-  )
+  );
 }
 
-export default VideoBanner
+export default VideoBanner;
